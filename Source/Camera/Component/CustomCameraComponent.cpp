b388972eb99aca10a3ffd762b24fa00d2f372984
lampardliu <lampardliu@tencent.com>
2023-11-14 20:35:21 +0800

    相机避障功能
    新添加

--- Source/Camera/Component/CustomCameraComponent.cpp
+++ Source/Camera/Component/CustomCameraComponent.cpp
@@ -1,10 +1,35 @@
 #include "CustomCameraComponent.h"
+#include "KismetTraceUtils.h"
 #include "Camera/Spring/CustomCameraSpring.h"
 #include "VisualLogger/VisualLogger.h"
+
+static int DrawCameraDebugInfo = 0;
+FAutoConsoleVariableRef CVar_DrawCameraDebugInfo(TEXT("Camera.DrawCameraDebugInfo"), DrawCameraDebugInfo, TEXT("True to draw camera debugging info."), ECVF_Cheat);
 
 UCustomCameraComponent::UCustomCameraComponent(const FObjectInitializer& ObjectInitializer)
 {
 	PrimaryComponentTick.bCanEverTick = true;
+
+	//相机避障射线初始化
+	CameraPenetrationAvoidanceRays.Add(FPenetrationAvoidanceRay(FRotator(0.0f, 0.0f, 0.0f), 1.0f, 10.f, 0, true));
+	CameraPenetrationAvoidanceRays.Add(FPenetrationAvoidanceRay(FRotator(0.0f, 3.0f, 0.0f), 1.f, 8.f, 0, false));
+	CameraPenetrationAvoidanceRays.Add(FPenetrationAvoidanceRay(FRotator(0.0f, -3.0f, 0.0f), 1.f, 8.f, 0, false));
+	CameraPenetrationAvoidanceRays.Add(FPenetrationAvoidanceRay(FRotator(0.0f, 6.0f, 0.0f), 0.9f, 8.f, 0, false));
+	CameraPenetrationAvoidanceRays.Add(FPenetrationAvoidanceRay(FRotator(0.0f, -6.0f, 0.0f), 0.9f, 8.f, 0, false));
+	CameraPenetrationAvoidanceRays.Add(FPenetrationAvoidanceRay(FRotator(0.0f, 9.0f, 0.0f), 0.8f, 8.f, 0, false));
+	CameraPenetrationAvoidanceRays.Add(FPenetrationAvoidanceRay(FRotator(0.0f, -9.0f, 0.0f), 0.8f, 8.f, 0, false));
+	CameraPenetrationAvoidanceRays.Add(FPenetrationAvoidanceRay(FRotator(0.0f, 12.0f, 0.0f), 0.7f, 8.f, 0, false));
+	CameraPenetrationAvoidanceRays.Add(FPenetrationAvoidanceRay(FRotator(0.0f, -12.0f, 0.0f), 0.7f, 8.f, 0, false));
+	CameraPenetrationAvoidanceRays.Add(FPenetrationAvoidanceRay(FRotator(8.0f, 0.0f, 0.0f), 1.0f, 8.f, 0, false));
+
+	//安全位置避障射线初始化
+	SafeLocPenetrationAvoidanceRays.Add(FPenetrationAvoidanceRay(FRotator(0.0f, 0.0f, 0.0f), 1.0f, 14.f, 0, true));
+
+	SafeLocationOffset = FVector(0.f, 0.f, 95.f);
+	
+	bValidateSafeLoc = true;
+	bPreventCameraPenetration = true;
+	bSampleCameraPenetrationByBlueprint = false;
 }
 
 void UCustomCameraComponent::BeginPlay()
@@ -23,7 +48,6 @@ void UCustomCameraComponent::BeginPlay()
 		HeightSpring->ResetSpring();
 	}
 }
-
 
 float UCustomCameraComponent::EvaluateRuntimeFloatCurve(const FRuntimeFloatCurve& Curve, const float Time)
 {
@@ -59,11 +83,11 @@ void UCustomCameraComponent::PreUpdateCameraStates()
 void UCustomCameraComponent::ComputeCameraSpringLen(const float DeltaTime, const float Speed) const
 {
 	//速度影响长度弹簧
-	ForwardSpring->SustainingForce = EvaluateRuntimeFloatCurve(SpeedAffectForwardSpringForceCurve,Speed);
+	ForwardSpring->SustainingForce = EvaluateRuntimeFloatCurve(SpeedAffectForwardSpringForceCurve, Speed);
 	ForwardSpring->UpdateSpring(DeltaTime);
 
 	//速度影响高度弹簧
-	HeightSpring->SustainingForce = EvaluateRuntimeFloatCurve(SpeedAffectHeightSpringForceCurve,Speed);
+	HeightSpring->SustainingForce = EvaluateRuntimeFloatCurve(SpeedAffectHeightSpringForceCurve, Speed);
 	HeightSpring->UpdateSpring(DeltaTime);
 }
 
@@ -149,6 +173,162 @@ void UCustomCameraComponent::ComputeCameraToWorld()
 	UE_VLOG_LOCATION(this,LogTemp,Log,CameraToWorld.GetLocation(),3,FColor::Red,TEXT("CameraLocation"));
 }
 
+void UCustomCameraComponent::CameraAvoidanceBlueprint_Implementation(const float DeltaTime) { }
+
+void UCustomCameraComponent::CameraAvoidance(const float DeltaTime)
+{
+	const UWorld* World = ViewTarget ? ViewTarget->GetWorld() : nullptr;
+	if(!IsValid(World)) { return; }
+
+	const FVector DesiredCamLoc = CameraToWorld.GetLocation();
+	FVector ValidatedCameraLocation = DesiredCamLoc;
+
+	if (bPreventCameraPenetration)
+	{
+		//默认可视点位置
+		const FVector IdealSafeLocation = ViewTarget->GetActorLocation() + ViewTarget->GetActorQuat().RotateVector(SafeLocationOffset);
+		FVector ValidatedSafeLocation = IdealSafeLocation;
+
+		//通过射线检测获取安全的可视点位置
+		if (bValidateSafeLoc)
+		{
+			float LastSafeLocBlockedPct = 1.f;
+			PreventCameraPenetration(ViewTarget, SafeLocPenetrationAvoidanceRays, ViewTarget->GetActorLocation(), IdealSafeLocation, DeltaTime, ValidatedSafeLocation, LastSafeLocBlockedPct, true);
+		}
+
+#if ENABLE_DRAW_DEBUG
+		if (bDrawDebugSafeLoc || DrawCameraDebugInfo)
+		{
+			//理想位置
+			::DrawDebugSphere(World, IdealSafeLocation, 12.f, 12.f, FColor::Yellow);
+			//安全位置
+			::DrawDebugSphere(World, ValidatedSafeLocation, 10.f, 10.f, FColor::White);
+		}		
+#endif
+		
+		PreventCameraPenetration(ViewTarget, CameraPenetrationAvoidanceRays, ValidatedSafeLocation, DesiredCamLoc, DeltaTime, ValidatedCameraLocation, LastPenetrationBlockedPct, false);
+	}
+
+	CameraToWorld.SetLocation(ValidatedCameraLocation);
+}
+
+
+void UCustomCameraComponent::PreventCameraPenetration(AActor* Target, TArray<FPenetrationAvoidanceRay>& Rays, const FVector& SafeLoc, const FVector& IdealCameraLoc, float DeltaTime, FVector& OutCameraLoc, float& DistBlockedPct, bool bPrimaryRayOnly)
+{
+	UWorld* const World = ViewTarget->GetWorld();
+
+	float HardBlockedPct = DistBlockedPct;
+	float SoftBlockedPct = DistBlockedPct;
+
+	FVector BaseRay = IdealCameraLoc - SafeLoc;
+	FRotationMatrix BaseRayMatrix(BaseRay.Rotation());
+	FVector BaseRayLocalFwd, BaseRayLocalRight, BaseRayLocalUp;
+	BaseRayMatrix.GetScaledAxes(BaseRayLocalFwd, BaseRayLocalRight, BaseRayLocalUp);
+	float DistBlockedPctThisFrame = 1.f;
+
+	FCollisionQueryParams SphereParams(SCENE_QUERY_STAT(CameraPenetration), false, Target);
+	FCollisionShape SphereShape = FCollisionShape::MakeSphere(0.f);
+
+	for (auto& Ray : Rays)
+	{
+		if (bPrimaryRayOnly && !Ray.bPrimaryRay) { continue; }
+
+		if (Ray.bEnabled)
+		{
+			if (Ray.FramesUntilNextTrace <= 0)
+			{
+				FVector RayTarget;
+				{
+					//Yaw值是围绕z轴旋转(摇头), Pitch是围绕y轴(点头)
+					FVector RotatedRay = BaseRay.RotateAngleAxis(Ray.AdjustmentRot.Yaw, BaseRayLocalUp);
+					RotatedRay = RotatedRay.RotateAngleAxis(Ray.AdjustmentRot.Pitch, BaseRayLocalRight);
+					RotatedRay = RotatedRay.RotateAngleAxis(Ray.AdjustmentRot.Roll, BaseRayLocalFwd);
+					RayTarget = SafeLoc + RotatedRay;
+				}
+
+				SphereShape.Sphere.Radius = Ray.Radius;
+				FHitResult Hit;
+				const FVector TraceStart = SafeLoc;
+				const FVector TraceEnd = RayTarget;
+				bool bHit = World->SweepSingleByChannel(Hit, TraceStart, TraceEnd, FQuat::Identity, ECC_Camera, SphereShape, SphereParams);
+				Ray.FramesUntilNextTrace = Ray.TraceInterval;
+
+#if ENABLE_DRAW_DEBUG
+				if (bDrawDebugPenetrationAvoidance)
+				{
+					::DrawDebugSphereTraceSingle(World, TraceStart, TraceEnd, Ray.Radius, EDrawDebugTrace::ForDuration, bHit, Hit, FColor::White, FColor::Red, 0.1f);
+				}
+#endif
+
+				if (bHit)
+				{
+					const float NewBlockPct = FMath::GetMappedRangeValueClamped(FVector2D(1.f, 0.f), FVector2D(Hit.Time, 1.f), Ray.WorldWeight);
+					DistBlockedPctThisFrame = FMath::Min(NewBlockPct, DistBlockedPctThisFrame);
+					Ray.FramesUntilNextTrace = 0;
+				}
+				
+				if (Ray.bPrimaryRay)
+				{
+					HardBlockedPct = DistBlockedPctThisFrame;
+				}
+				else
+				{
+					SoftBlockedPct = DistBlockedPctThisFrame;
+				}
+			}
+			else
+			{
+				--Ray.FramesUntilNextTrace;
+			}
+		}
+	}
+
+	if (DistBlockedPct < DistBlockedPctThisFrame)
+	{
+		if (PenetrationBlendOutTime > DeltaTime)
+		{
+			DistBlockedPct = DistBlockedPct + DeltaTime / PenetrationBlendOutTime * (DistBlockedPctThisFrame - DistBlockedPct);
+		}
+		else
+		{
+			DistBlockedPct = DistBlockedPctThisFrame;
+		}
+	}
+	else
+	{
+		if (DistBlockedPct > HardBlockedPct)
+		{
+			DistBlockedPct = HardBlockedPct;
+		}
+		else if (DistBlockedPct > SoftBlockedPct)
+		{
+			if (PenetrationBlendInTime > DeltaTime)
+			{
+				DistBlockedPct = DistBlockedPct - DeltaTime / PenetrationBlendInTime * (DistBlockedPct - SoftBlockedPct);
+			}
+			else
+			{
+				DistBlockedPct = SoftBlockedPct;
+			}
+		}
+	}
+
+	DistBlockedPct = FMath::Clamp<float>(DistBlockedPct, 0.f, 1.f);
+	if (DistBlockedPct < KINDA_SMALL_NUMBER)
+	{
+		DistBlockedPct = 0.f;
+	}
+
+	if (DistBlockedPct < 1.f)
+	{
+		OutCameraLoc = SafeLoc + (IdealCameraLoc - SafeLoc) * DistBlockedPct;
+	}
+	else
+	{
+		OutCameraLoc = IdealCameraLoc;
+	}
+}
+
 void UCustomCameraComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
@@ -157,30 +337,27 @@ void UCustomCameraComponent::TickComponent(float DeltaTime, ELevelTick TickType,
 
 	const APlayerController* PC = Cast<APlayerController>(ViewTarget->GetController());
 	const float Velocity = ViewTarget->GetVelocity().Size() * 0.01f * 3.6f;
-
-	//增加帧率是为了让相机的移动和转向更加平滑
-	int32 ComputeCounts = 1;
-	if(DeltaTime > MinTickGap) { ComputeCounts = FMath::CeilToInt(DeltaTime/MinTickGap); }
-	for(int32 i=0; i < ComputeCounts; i++)
-	{
-		if (!LastControlRotation.Equals(PC->GetControlRotation(), 0.1f) || Velocity < AutoFollowCameraSpeed)
-		{
-			ControlTimeRemaining = DelayControlTime;
-		}
-		
-		if (ControlTimeRemaining > 0.f)
-		{
-			ControlTimeRemaining -= MinTickGap;
-			TempSphereLerpAlpha = AutoFollowSphereLerpAlpha;
-		}
 	
-		PreUpdateCameraStates();
-		ComputeCameraSpringLen(MinTickGap,Velocity);
-		ComputePivotToWorld();
-		ComputeLookAtPos();
-		ComputeCameraToWorld();
-		LastControlRotation = PC->GetControlRotation();
+	if (!LastControlRotation.Equals(PC->GetControlRotation(), 0.1f) || Velocity < AutoFollowCameraSpeed)
+	{
+		ControlTimeRemaining = DelayControlTime;
+	}
+	if (ControlTimeRemaining > 0.f)
+	{
+		ControlTimeRemaining -= MinTickGap;
+		TempSphereLerpAlpha = AutoFollowSphereLerpAlpha;
 	}
 	
+	PreUpdateCameraStates();
+	int32 ComputeCounts = 1;
+	if(DeltaTime > MinTickGap) { ComputeCounts = FMath::CeilToInt(DeltaTime/MinTickGap); }
+	for(int32 i=0; i < ComputeCounts; i++) { ComputeCameraSpringLen(MinTickGap,Velocity); }
+	ComputePivotToWorld();
+	ComputeLookAtPos();
+	ComputeCameraToWorld();
+	CameraAvoidance(DeltaTime);
+	CameraAvoidanceBlueprint(DeltaTime);
+	
+	LastControlRotation = PC->GetControlRotation();
 	GetOwner()->SetActorTransform(CameraToWorld);
 }
--- Source/Camera/Component/CustomCameraComponent.h
+++ Source/Camera/Component/CustomCameraComponent.h
@@ -7,6 +7,47 @@
 class UCustomCameraSpring;
 
 USTRUCT(BlueprintType)
+struct FPenetrationAvoidanceRay
+{
+	GENERATED_BODY()
+
+	//当前检测是否起效
+	UPROPERTY(EditAnywhere, Category = PenetrationAvoidanceRay)
+	bool bEnabled = true;
+	
+	//是否是主要的检测射线
+	UPROPERTY(EditAnywhere, Category = PenetrationAvoidanceRay)
+	bool bPrimaryRay = false;
+	
+	//相对主检测射线的旋转
+	UPROPERTY(EditAnywhere, Category=PenetrationAvoidanceRay)
+	FRotator AdjustmentRot;
+
+	//射线检测到障碍物的时候,该条射线的权重
+	UPROPERTY(EditAnywhere, Category= PenetrationAvoidanceRay)
+	float WorldWeight = 0.f;
+
+	//检测射线的半径,SweepSingleByChannel内的CollisionShape内使用Sphere时的半径
+	UPROPERTY(EditAnywhere, Category= PenetrationAvoidanceRay)
+	float Radius = 0.f;
+
+	//如果当前帧未检测出碰撞,那么距离下一次检测的间隔(单位：帧)
+	UPROPERTY(EditAnywhere, Category= PenetrationAvoidanceRay)
+	int32 TraceInterval = 2;
+
+	UPROPERTY(transient)
+	int32 FramesUntilNextTrace = 0;
+
+	FPenetrationAvoidanceRay(): AdjustmentRot(ForceInit){ }
+	FPenetrationAvoidanceRay(const FRotator& InAdjustmentRot,  float InWorldWeight, float InRadius, int32 InTraceInterval, bool bInPrimaryRay)
+		: AdjustmentRot(InAdjustmentRot)
+		, WorldWeight(InWorldWeight)
+		, Radius(InRadius)
+		, TraceInterval(InTraceInterval)
+		, bPrimaryRay(bInPrimaryRay) { }
+};
+
+USTRUCT(BlueprintType)
 struct CAMERA_API FCustomCameraOffset
 {
 	GENERATED_USTRUCT_BODY()
@@ -53,6 +94,11 @@ protected:
 	void ComputePivotToWorld();
 	void ComputeLookAtPos();
 	void ComputeCameraToWorld();
+	void CameraAvoidance(const float DeltaTime);
+	UFUNCTION(Blueprintable,BlueprintNativeEvent)
+	void CameraAvoidanceBlueprint(const float DeltaTime);
+	
+	virtual void PreventCameraPenetration(AActor* Target, TArray<FPenetrationAvoidanceRay>& Rays, const FVector& SafeLoc, const FVector& IdealCameraLoc, float DeltaTime, FVector& OutCameraLoc, float& DistBlockedPct, bool bPrimaryRayOnly = false);
 
 public:	
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
@@ -62,7 +108,7 @@ protected:
 	float MinTickGap = 0.01f;
 	UPROPERTY(EditAnywhere, Category = "Custom Param")
 	float DelayControlTime = 0.8f;
-	//速度的大于当前值,并且没有使用鼠标控制的时候,相机自动回正
+	//速度大于当前值并且没有使用鼠标控制的时候,相机自动回正
 	UPROPERTY(EditAnywhere, Category = "Custom Param")
 	float AutoFollowCameraSpeed = 5.f;
 	//相机跟随状态下,回正的速度
@@ -85,6 +131,38 @@ protected:
 	//速度影响HeightSpring弹簧曲线，横轴(速度KM/h)，纵轴:力(N)
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Custom Param")
 	FRuntimeFloatCurve SpeedAffectHeightSpringForceCurve;
+	
+	//相机避障检测参数
+	UPROPERTY(EditAnywhere, Category = "PenetrationAvoidance")
+	TArray<FPenetrationAvoidanceRay> CameraPenetrationAvoidanceRays;
+	//可视点避障检测参数
+	UPROPERTY(EditAnywhere, Category = "PenetrationAvoidance")
+	TArray<FPenetrationAvoidanceRay> SafeLocPenetrationAvoidanceRays;
+
+	//可视点偏移（相对ViewTarget位置）
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PenetrationAvoidance")
+	FVector SafeLocationOffset;
+
+	//蓝图内简单实现相机避障
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PenetrationAvoidance")
+	uint8 bSampleCameraPenetrationByBlueprint:1;
+	//是否开启可视点避障检测
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PenetrationAvoidance")
+	uint8 bValidateSafeLoc:1;
+	//是否开启相机臂章检测
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PenetrationAvoidance")
+	uint8 bPreventCameraPenetration : 1;
+
+	//相机从理想位置靠近到安全点的混入时间(有障碍物,相机往可视点靠近的混入时间)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PenetrationAvoidance")
+	float PenetrationBlendInTime = 0.15f;
+	//相机从安全点靠近理想位置的混出时间(没有障碍物,相机拉回去的混出时间)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PenetrationAvoidance")
+	float PenetrationBlendOutTime = 0.25f;
+
+	//上一次相机渗透百分比
+	UPROPERTY(transient)
+	float LastPenetrationBlockedPct = 1.f;
 
 public:
 	UPROPERTY(BlueprintReadOnly)
@@ -103,7 +181,7 @@ public:
 	FTransform PivotToWorld = FTransform::Identity;
 	UPROPERTY(BlueprintReadOnly)
 	FTransform LookAtToWorld = FTransform::Identity;
-	UPROPERTY(BlueprintReadOnly)
+	UPROPERTY(BlueprintReadWrite)
 	FTransform CameraToWorld = FTransform::Identity;
 	UPROPERTY(BlueprintReadOnly)
 	FTransform LastPivotToWorld = FTransform::Identity;
@@ -112,4 +190,12 @@ private:
 	float ControlTimeRemaining = 0.f;
 	float TempSphereLerpAlpha = 0.1f;
 	FRotator LastControlRotation = FRotator::ZeroRotator;
+
+protected:
+	/** When enabled, debug visuals will be rendered for the camera safe location, where penetration avoidance ray traces originate */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
+	bool bDrawDebugSafeLoc = false;
+	/** When enabled, debug visuals will be rendered for the penetration avoidance ray traces */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
+	bool bDrawDebugPenetrationAvoidance = false;
 };
